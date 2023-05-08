38b2edc17e210ec63a987ff4958ed8689acea49a
stgran66 <stanislaw.grygoriew@gmail.com>
2023-05-08 18:44:15 +0200

    refactored time input logic

--- src/components/TimeInput.tsx
+++ src/components/TimeInput.tsx
@@ -14,28 +14,22 @@ export const TimeInput = (props: any) => {
     // Check hours to be from 0 to 23
     if (hh.length === 1 && hh[0] > 2) {
       hh = `0${hh[0]}`;
-    } else if (hh.length === 2) {
-      if (Number(hh) > 23) {
-        hh = '23';
-      }
+    } else if (hh.length === 2 && Number(hh) > 23) {
+      hh = '23';
     }
 
     // Check minutes to be from 0 to 59
     if (mm.length === 1 && mm[0] >= 6) {
       mm = `0${mm[0]}`;
-    } else if (mm.length === 2) {
-      if (Number(mm) > 59) {
-        mm = '59';
-      }
+    } else if (mm.length === 2 && Number(mm) > 59) {
+      mm = '59';
     }
 
     // Check seconds to be from 0 to 59
     if (ss.length === 1 && ss[0] >= 6) {
       ss = `0${ss[0]}`;
-    } else if (ss.length === 2) {
-      if (Number(ss) > 59) {
-        ss = '59';
-      }
+    } else if (ss.length === 2 && Number(ss) > 59) {
+      ss = '59';
     }
     // Return formatted time
     return format(`${hh}${mm}${ss}`);
